0d3c9c1e7c67de7fc61b5b3343c7928cb956a18b
mikeson <mikeson.jnr@gmail.com>
2023-05-31 16:42:55 +0100

    Set up html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Battleship'
+    })
+  ],
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"]
+    }, {
+      test: /\.(png|svg|jpg|jpeg|gif|mp4)$/i,
+      type: 'asset/resource',
+    }]
+  }
 };
