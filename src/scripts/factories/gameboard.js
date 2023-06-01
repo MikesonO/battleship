ae36d0224f67054e37bb8b68d2517f2f10f57c10
mikeson <mikeson.jnr@gmail.com>
2023-06-01 16:07:13 +0100

    Add receiveAttack function

--- src/scripts/factories/gameboard.js
+++ src/scripts/factories/gameboard.js
@@ -3,7 +3,7 @@ export class Gameboard {
     this.grid = this.createGrid();
   }
 
-  // Creates a 10 x 10 grid with null values
+  // Creates a 10 row 10 grid with null values
   createGrid() {
     const grid = [];
     for (let row = 0; row < 10; row++) {
@@ -16,46 +16,53 @@ export class Gameboard {
     return grid;
   }
 
-  placeShip(ship, x, y, isVertical) {
+  placeShip(ship, row, col, isVertical) {
     const shipLength = ship.length;
     const shipSections = [];
 
     // Check if the ship can be placed within the boundaries of the grid
     if (isVertical) {
-      if (y + shipLength > 10) {
+      if (col + shipLength > 10) {
         throw new Error('Ship placement out of bounds');
       }
 
       // Check if there are any obstructions at the specified coordinates
-      for (let i = y; i < y + shipLength; i++) {
-        if (this.grid[i][x]) {
+      for (let i = col; i < col + shipLength; i++) {
+        if (this.grid[i][row]) {
           throw new Error('Ship placement obstructed');
         }
-        shipSections.push({x, y: i});
+        shipSections.push({row, col: i});
       }
     } else {
-      if (x + shipLength > 10) {
+      if (row + shipLength > 10) {
         throw new Error('Ship placement out of bounds');
       }
 
       // Check if there are any obstructions at the specified coordinates
-      for (let i = x; i < x + shipLength; i++) {
-        if (this.grid[y][i]) {
+      for (let i = row; i < row + shipLength; i++) {
+        if (this.grid[col][i]) {
           throw new Error('Ship placement obstructed');
         }
-        shipSections.push({x: i, y});
+        shipSections.push({row: i, col});
       }
     }
 
     // Place the ship sections on the grid
-    shipSections.forEach(({x, y}) => {
-      this.grid[y][x] = ship;
+    shipSections.forEach(({row, col}) => {
+      this.grid[col][row] = ship;
     });
   }
 
 
-  receiveAttack() {
-    
+  receiveAttack(row, col) {
+    const target = this.grid[row][col];
+    if (target === null){
+      // Record the coordinates of the missed shot
+      this.grid[row][col] = 'miss';
+    } else if (target instanceof Ship) {
+      // Call the 'hit' function on the ship
+      target.hit(row, col);
+    }
   }
 
 
