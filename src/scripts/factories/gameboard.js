17d846e9cde8d44e7061b1d5f9d49758e802b5b5
mikeson <mikeson.jnr@gmail.com>
2023-06-01 11:59:58 +0100

    Add placeShip function to Gameboard class

--- src/scripts/factories/gameboard.js
+++ src/scripts/factories/gameboard.js
@@ -16,5 +16,42 @@ export class Gameboard {
     return grid;
   }
 
+  placeShip(ship, x, y, isVertical) {
+    const shipLength = ship.length;
+    const shipSections = [];
+
+    // Check if the ship can be placed within the boundaries of the grid
+    if (isVertical) {
+      if (y + shipLength > 10) {
+        console.log('Ship placement out of bounds');
+      }
+
+      // Check if there are any obstructions at the specified coordinates
+      for (let i = y; i < y + shipLength; i++) {
+        if (this.grid[i][x]) {
+          console.log('Ship placement obstructed');
+        }
+        shipSections.push({x, y: i});
+      }
+    } else {
+      if (x + shipLength > 10) {
+        console.log('Ship placement out of bounds');
+      }
+
+      // Check if there are any obstructions at the specified coordinates
+      for (let i = x; i < x + shipLength; i++) {
+        if (this.grid[y][i]) {
+          console.log('Ship placement obstructed');
+        }
+        shipSections.push({x: i, y});
+      }
+    }
+
+    // Place the ship sections on the grid
+    shipSections.forEach(({x, y}) => {
+      this.grid[y][x] = ship;
+    });
+  }
+
 
 };
