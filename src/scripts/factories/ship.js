469b3088890f329bfcd56d96ea8e4a515a7a39b4
mikeson <mikeson.jnr@gmail.com>
2023-06-01 11:59:46 +0100

    Add rotate function to Ship class

--- src/scripts/factories/ship.js
+++ src/scripts/factories/ship.js
@@ -2,6 +2,7 @@ export class Ship {
   constructor(length) {
     this.length = length;
     this.hits = Array(length).fill(false); // sets each element in the array to false.
+    this.isVerticle = false;
   }
 
   hit(position) {
@@ -15,4 +16,13 @@ export class Ship {
   isSunk() {
     return this.hits.every((hit) => hit);
   }
+
+  rotate() {
+    if (!this.isVertical) {
+      this.isVertical = true;
+    } else {
+      this.isVertical = false;
+    }
+  }
+  
 }
