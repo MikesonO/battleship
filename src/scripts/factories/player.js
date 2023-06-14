98174991fcb4475d8308b6429b9d6721a900460f
mike.osinowo <mikeson.osinowo@incisivemedia.com>
2023-06-14 11:51:14 +0100

    Add makeRandomPlay function

--- src/scripts/factories/player.js
+++ src/scripts/factories/player.js
@@ -2,6 +2,7 @@ export class Player {
   constructor(name){
     this.name = name;
     this.turn = true;
+    this.previousAttacks = [];
   }
 
   setName(name){
@@ -22,6 +23,34 @@ export class Player {
     if (this.turn){
       this.turn = false;
     }
+  }
+
+  makeRandomPlay(gameboard) {
+    const rows = gameboard.grid.length;
+    const cols = gameboard.grid[0].length;
+
+    let row, col;
+    let validMove = false;
+
+    while (!validMove) {
+      row = Math.floor(Math.random() * rows);
+      col = Math.floor(Math.random() * cols);
+
+      // Check if the move has already been played
+      const isRepeatedMove = this.previousAttacks.some((attack) => {
+        return attack.row === row && attack.col === col;
+      });
+
+      if (!isRepeatedMove) {
+        validMove = true;
+      }
+    }
+
+    // Record the attack
+    this.previousAttacks.push({ row, col });
+
+    // Make the attack on the gameboard
+    gameboard.receiveAttack(row, col);
   }
   
 
