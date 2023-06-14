9c3247ca30681084493035e48cec7a54c6782741
mike.osinowo <mikeson.osinowo@incisivemedia.com>
2023-06-14 16:22:27 +0100

    Set up the gameLoop function

---
+++ src/scripts/game/game.js
@@ -0,0 +1,17 @@
+import { Player } from './Player.js';
+import { Gameboard } from './Gameboard.js';
+
+// Create players and gameboards
+const player1 = new Player('Player 1');
+const computer = new Player('Computer');
+
+const player1Gameboard = new Gameboard();
+const computerGameboard = new Gameboard();
+
+// Game logic
+function gameLoop() {
+ 
+}
+
+// Start the game loop
+gameLoop();
